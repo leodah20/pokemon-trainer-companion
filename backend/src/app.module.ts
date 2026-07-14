252ba5599ab481fc62bb931d4e875f6b55973dda
Leonardo Cordeiro <134634613+leodah20@users.noreply.github.com>
2026-07-14 11:55:19 -0300

    feat: Integrate Species and Prisma modules into AppModule; set up Swagger documentation in main.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SpeciesModule } from './presentation/species/speciesModule';
+import { PrismaModule } from './presentation/prisma/prismaModule';
 
 @Module({
-  imports: [],
+  imports: [SpeciesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './presentation/interceptors/logging.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api', { exclude: ['/'] });
   app.useGlobalInterceptors(new LoggingInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Pokémon Trainer Companion API')
+    .setDescription('Backend API for the Pokémon Trainer Companion app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
